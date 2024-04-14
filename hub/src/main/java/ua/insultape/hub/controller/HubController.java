5385d4cca0dfee78ccda3adce61e7c7e70de8ba9
Vitalii Hutov <fake.gutov@gmail.com>
2024-04-14 16:36:38 +0300

    Lab-3 update endpoint

--- hub/src/main/java/ua/insultape/hub/controller/HubController.java
+++ hub/src/main/java/ua/insultape/hub/controller/HubController.java
@@ -3,12 +3,14 @@ package ua.insultape.hub.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.insultape.hub.dto.ProcessedAgentData;
 import ua.insultape.hub.service.AccumulationService;
 
 @Slf4j
 @RestController
+@RequestMapping("/hub")
 public class HubController {
 
     private final AccumulationService accumulationService;
